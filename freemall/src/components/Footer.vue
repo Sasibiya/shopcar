6a17ef96c3f22a424e4720477fe223521324c066
ironman <wukaiat.10343@qq.com>
2020-07-29 22:15:01 +0800

    shopcar

---
+++ freemall/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+   <!-- 底部内容 -->
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__secondary__nav">
+            <span><a href="#" target="_blank">admin</a>|<a
+                href=# target="_blank">用户协议</a>|<a
+                href="https://coding.imooc.com/class/236.html" target="_blank">问题反馈</a>|<a
+                href="#" target="_blank">政企服务</a></span>
+            <p>
+              <a href="#">关于我们</a>|
+              <a href="#">条款和条件</a>|
+              <a href="#">隐私政策</a>
+            </p>
+            <p>Copyright © 2020 admin 版权所有</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer',
+  props: {
+  }
+}
+</script>
+
---
+++ freemall/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	lintOnSave:false,
+	publicPath:'',
+	devServer:{
+		host:'localhost',
+		port:8080,
+		proxy:{
+			'/mock':{
+				target:'http://localhost:8080',
+				ws:false,
+				//
+				changeOrigin:false
+			}
+		}
+	}
+}
